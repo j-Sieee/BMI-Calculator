7c13f108f91f7007327c2a4ff9700eca9ac598b1
iamjessie <jessieagapito08@gmail.com>
2020-07-21 16:45:16 +0800

    Added calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   IonApp,
   IonContent,
@@ -32,43 +32,64 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonHeader>
-      <IonToolbar color="success">
-        <IonTitle>BMI Calculator</IonTitle>
-      </IonToolbar>
-    </IonHeader>
+const App: React.FC = () => {
+  const inputHeightRef = useRef<HTMLIonInputElement>(null);
+  const inputWeightRef = useRef<HTMLIonInputElement>(null);
 
-    <IonContent className="ion-padding">
-      <IonGrid>
-        <IonRow>
-          <IonCol>
-            <IonLabel>Your Height: </IonLabel>
-            <IonInput type="number"></IonInput>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonLabel>Your Weight: </IonLabel>
-            <IonInput type="number"></IonInput>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonButton color="success">
-              <IonLabel>Calculate</IonLabel>
-            </IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton color="success">
-              <IonLabel>Clear Inputs</IonLabel>
-            </IonButton>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </IonContent>
-  </IonApp>
-);
+  const calcbutton = () => {
+    const enteredHeight = inputHeightRef.current!.value;
+    const enteredWeight = inputWeightRef.current!.value;
+
+    if (
+      !enteredHeight ||
+      !enteredWeight ||
+      enteredWeight <= 0 ||
+      enteredHeight <= 0
+    ) {
+      return;
+    }
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
+
+    console.log(bmi);
+  };
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar color="success">
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonLabel>Your Height: </IonLabel>
+              <IonInput ref={inputHeightRef} type="number"></IonInput>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonLabel>Your Weight: </IonLabel>
+              <IonInput ref={inputWeightRef} type="number"></IonInput>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton onClick={calcbutton} color="success">
+                <IonLabel>Calculate</IonLabel>
+              </IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton color="success">
+                <IonLabel>Clear Inputs</IonLabel>
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
